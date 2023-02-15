9520ced7f6661ea3cdc4f20a20441e1ca8599248
Keaton <keanaido19@gmail.com>
2023-02-15 10:03:08 +0200

    Created Project Skeleton

---
+++ src/main/java/za/co/bbd/atc/propertymanagement/PropertymanagementApplication.java
@@ -0,0 +1,13 @@
+package za.co.bbd.atc.propertymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/bbd/atc/propertymanagement/PropertymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.atc.propertymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
